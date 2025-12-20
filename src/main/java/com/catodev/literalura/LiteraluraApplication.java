e5ee917a4673815ca511a11645e8f4cee1e0cab5
Catriel42 <catopereira72@gmail.com>
2025-12-20 00:24:22 -0400

    feat: Adding a request to basic endpoint

--- src/main/java/com/catodev/literalura/LiteraluraApplication.java
+++ src/main/java/com/catodev/literalura/LiteraluraApplication.java
@@ -1,13 +1,28 @@
 package com.catodev.literalura;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello World from Spring Boot!");
+        Service service = new Service();
+        try {
+            System.out.println(service.get());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
